246dc8b76c700fec14f6d4f4072c69df52f283b5
piotrek-pl <125832683+piotrek-pl@users.noreply.github.com>
2024-08-27 00:16:33 +0200

    microphone law leak

--- app/src/main/java/com/example/commandserviceapp/BootWorker.kt
+++ app/src/main/java/com/example/commandserviceapp/BootWorker.kt
@@ -1,10 +1,16 @@
 import android.content.Context
+import android.content.Intent
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.example.commandserviceapp.CommandService
 
 class BootWorker(context: Context, workerParams: WorkerParameters) : Worker(context, workerParams) {
+
     override fun doWork(): Result {
-        // Tutaj wykonaj zadanie w tle
+        // Uruchomienie usługi w trybie pierwszoplanowym
+        val intent = Intent(applicationContext, CommandService::class.java)
+        applicationContext.startForegroundService(intent)
+
         return Result.success()
     }
 }
--- app/src/main/java/com/example/commandserviceapp/MainActivity.kt
+++ app/src/main/java/com/example/commandserviceapp/MainActivity.kt
@@ -5,12 +5,5 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        // Uruchomienie usługi
-        val serviceIntent = Intent(this, CommandService::class.java)
-        startForegroundService(serviceIntent)
-        // Zamknięcie aktywności
-        //finish()
-    }
+
 }
